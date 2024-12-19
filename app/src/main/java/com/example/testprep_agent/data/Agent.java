6a2bbd9a3191657e575ce5a6e8635a25ca033e9f
Alexutzi10 <georgealexandrusurugiu@gmail.com>
2024-12-19 15:53:56 +0200

    Room integration in classes

--- app/src/main/java/com/example/testprep_agent/data/Agent.java
+++ app/src/main/java/com/example/testprep_agent/data/Agent.java
@@ -1,11 +1,26 @@
 package com.example.testprep_agent.data;
 
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
 import java.util.Date;
 
+@Entity(tableName = "agents")
 public class Agent {
+    @PrimaryKey(autoGenerate = true)
+    private long id;
     private String name;
     private int payment;
     private Date employmentDate;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -31,10 +46,19 @@ public class Agent {
         this.employmentDate = employmentDate;
     }
 
+    @Ignore
     public Agent() {
     }
 
+    @Ignore
     public Agent(String name, int payment, Date employmentDate) {
+        this.name = name;
+        this.payment = payment;
+        this.employmentDate = employmentDate;
+    }
+
+    public Agent(long id, String name, int payment, Date employmentDate) {
+        this.id = id;
         this.name = name;
         this.payment = payment;
         this.employmentDate = employmentDate;
@@ -43,7 +67,8 @@ public class Agent {
     @Override
     public String toString() {
         return "Agent{" +
-                "name='" + name + '\'' +
+                "id=" + id +
+                ", name='" + name + '\'' +
                 ", payment=" + payment +
                 ", employmentDate=" + employmentDate +
                 '}';
--- app/src/main/java/com/example/testprep_agent/data/DateConverter.java
+++ app/src/main/java/com/example/testprep_agent/data/DateConverter.java
@@ -1,5 +1,7 @@
 package com.example.testprep_agent.data;
 
+import androidx.room.TypeConverter;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -8,6 +10,7 @@ import java.util.Locale;
 public class DateConverter {
     public static final SimpleDateFormat FORMATTER = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
 
+    @TypeConverter
     public static String fromDate(Date value) {
         if (value == null) {
             return null;
@@ -16,6 +19,7 @@ public class DateConverter {
         }
     }
 
+    @TypeConverter
     public static Date toDate(String value) {
         try {
             return FORMATTER.parse(value);
